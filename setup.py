172d02d7939ab29a490cc469740d481a02eebba3
aymanalz <aalzraiee@usgs.gov>
2020-12-30 10:01:47 -0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='gw_utils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Ayman Alzraiee',
+      author_email='ayman.alzraiee@gmail.com',    
+      packages = find_packages()
+     )
